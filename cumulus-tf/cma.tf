522698a7472773e18dc1d91f80fd718fa96ac5df
Mark Boyd <markdavidboyd@gmail.com>
2021-02-17 16:58:36 -0500

    fix CMA deployment

--- cumulus-tf/cma.tf
+++ cumulus-tf/cma.tf
@@ -3,14 +3,20 @@ resource "null_resource" "fetch_CMA_release" {
     always_run = timestamp()
   }
   provisioner "local-exec" {
-    command = "curl -L -o cumulus-message-adapter.zip https://github.com/nasa/cumulus-message-adapter/releases/download/${var.cumulus_message_adapter_version}/cumulus-message-adapter.zip"
+    command = "curl -L -o cumulus-message-adapter.zip https://github.com/nasa/cumulus-message-adapter/releases/download/v${var.cumulus_message_adapter_version}/cumulus-message-adapter.zip"
   }
 }
 
+resource "aws_s3_bucket_object" "cma_release" {
+  depends_on = [null_resource.fetch_CMA_release]
+  bucket     = var.system_bucket
+  key        = "cumulus-message-adapter-${var.cumulus_message_adapter_version}.zip"
+  source     = "${path.module}/cumulus-message-adapter.zip"
+}
+
 resource "aws_lambda_layer_version" "cma_layer" {
-  depends_on = [ null_resource.fetch_CMA_release ]
-  # source_code_hash = filebase64sha256("${path.module}/cumulus-message-adapter.zip")
-  filename = "${path.module}/cumulus-message-adapter.zip"
+  s3_bucket  = var.system_bucket
+  s3_key     = aws_s3_bucket_object.cma_release.key
   layer_name = "${var.prefix}-CMA-layer"
   description = "Lambda layer for Cumulus Message Adapter ${var.cumulus_message_adapter_version}"
 }
