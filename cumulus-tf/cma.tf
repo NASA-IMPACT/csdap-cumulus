2e6e625378a091a737c87ef77f9ae4823dac1848
Mark Boyd <markdavidboyd@gmail.com>
2021-02-17 13:20:19 -0500

    refactor deploment

--- cumulus-tf/cma.tf
+++ cumulus-tf/cma.tf
@@ -9,7 +9,7 @@ resource "null_resource" "fetch_CMA_release" {
 
 resource "aws_lambda_layer_version" "cma_layer" {
   depends_on = [ null_resource.fetch_CMA_release ]
-  source_code_hash = filebase64sha256("${path.module}/cumulus-message-adapter.zip")
+  # source_code_hash = filebase64sha256("${path.module}/cumulus-message-adapter.zip")
   filename = "${path.module}/cumulus-message-adapter.zip"
   layer_name = "${var.prefix}-CMA-layer"
   description = "Lambda layer for Cumulus Message Adapter ${var.cumulus_message_adapter_version}"
--- init-tf-vars.sh
+++ init-tf-vars.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 export TF_VAR_prefix=$PREFIX
-export TF_VAR_system_bucket="$PREFIX-internal"
