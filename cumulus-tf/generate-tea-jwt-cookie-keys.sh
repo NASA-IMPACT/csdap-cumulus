b2345516054cb731988491668a232a3efb3fe8d6
Chuck Daniels <chuck@developmentseed.org>
2021-06-09 10:55:49 -0400

    Avoid ssh-keygen prompt; use blank passphrase

--- cumulus-tf/generate-tea-jwt-cookie-keys.sh
+++ cumulus-tf/generate-tea-jwt-cookie-keys.sh
@@ -6,7 +6,7 @@ _keyfile=${_ssh_dir}/thin-egress-app-jwt-cookie.key
 if [[ ! -f ${_keyfile} ]]; then
   # shellcheck disable=SC2174
   mkdir -p -m 0700 "${_ssh_dir}"
-  ssh-keygen -q -t rsa -b 4096 -m PEM -f "${_keyfile}"
+  ssh-keygen -q -t rsa -b 4096 -m PEM -f "${_keyfile}" -N ""
 fi
 
 echo "{\"rsa_priv_key\":\"$(openssl base64 -in "${_keyfile}" -A)\",\"rsa_pub_key\":\"$(openssl base64 -in "${_keyfile}.pub" -A)\"}"
