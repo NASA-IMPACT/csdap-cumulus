0710d25997d0bce2457857fd2b814eef19f85057
Chuck Daniels <chuck@developmentseed.org>
2023-02-02 16:35:26 -0500

    Upgrade AWS Aurora Postgres from 10.18 to 11.13 (#112)
    - Bumped `engine_version` from 10.18 to 11.13
    - Removed unnecessary `variables.tf` file and inlined values
    - Fixed `plan-*` Makefile targets to allow interaction
    
    Fixes #110

--- app/stacks/rds-cluster/main.tf
+++ app/stacks/rds-cluster/main.tf
@@ -13,16 +13,17 @@ module "rds_cluster" {
 
   cluster_identifier       = "${var.prefix}-rds-serverless"
   db_admin_password        = random_password.db_password.result
-  db_admin_username        = var.db_admin_username
-  deletion_protection      = var.deletion_protection
-  engine_version           = var.engine_version
+  db_admin_username        = "postgres"
+  deletion_protection      = true
+  engine_version           = "11.13"
+  parameter_group_family   = "aurora-postgresql11"
   permissions_boundary_arn = local.permissions_boundary_arn
   prefix                   = var.prefix
-  provision_user_database  = var.provision_user_database
+  provision_user_database  = true
   rds_user_password        = random_password.db_password.result
   region                   = data.aws_region.current.name
-  snapshot_identifier      = var.snapshot_identifier
+  snapshot_identifier      = null
   subnets                  = module.vpc.subnets.ids
-  tags                     = merge(var.tags, { Deployment = var.prefix })
+  tags                     = { Deployment = var.prefix }
   vpc_id                   = module.vpc.vpc_id
 }
