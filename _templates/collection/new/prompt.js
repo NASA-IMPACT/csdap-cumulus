769815b4450e00236239f5e500aa996c33fa5f2c
Kris Stanton <ksnimpact@gmail.com>
2023-04-27 19:15:08 -0500

    -Made a new Hygen rule for making brand new collections -Defined a new Data Management Item for WV01_Pan_L1B. 	-There was already a maxar provider - no change on providers list. 	-Created new file, WV01_Pan_L1B___1.json by using the new colleciton in Hygen 	-Created a the real Sample Granule 	-Created rules for WV01_Pan_L1B for years 2007 to 2022 #99

---
+++ _templates/collection/new/prompt.js
@@ -0,0 +1,20 @@
+// see types of prompts:
+// https://github.com/enquirer/enquirer/tree/master/examples
+//
+module.exports = [
+  {
+    type: 'input',
+    name: 'collectionName',
+    message: "Collection name:"
+  },
+  {
+    type: 'input',
+    name: 'collectionVersion',
+    message: "Collection version:"
+  },
+  {
+    type: 'input',
+    name: 'sampleFilenamePrefix',
+    message: "Sample Filename Prefix"
+  }
+]
