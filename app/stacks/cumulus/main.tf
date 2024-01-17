be0a82e67ce905759b209729610f3b800ae040fd
Chuck Daniels <chuck@developmentseed.org>
2024-01-17 16:14:04 -0500

    Increase RDS connection pool max capacity in Prod (#332)
    Also:
    
    - increase step function state concurrency, which the increased
      connection pool capacity should now handle
    - add missing retry configuration for steps in ingestion workflow
      to increase resilience
    - set duplicateHandling to "skip" for both WV03 collections to
      allow rerunning ingestion without reingesting existing granules
    - reduce background queue message limit to avoid reaching concurrency
      quota for Lambda executions
    - increase queue throttle to increase ingestion rate (in combination with
      previous point, this should still keep us within Lamba quota)
    - reduce default granule batch size for queuing to 1000 to ensure we
      won't encounter QueueGranules timeouts with "skip" duplicate handling
      (due to extra DB queries by DiscoverGranules to check for duplicates)

--- app/stacks/cumulus/main.tf
+++ app/stacks/cumulus/main.tf
@@ -161,9 +161,9 @@ resource "aws_cloudwatch_event_target" "background_job_queue_watcher" {
   arn  = module.cumulus.sqs2sfThrottle_lambda_function_arn
   input = jsonencode(
     {
-      messageLimit = 1000
+      messageLimit = 300
       queueUrl     = aws_sqs_queue.background_job_queue.id
-      timeLimit    = 60
+      timeLimit    = 30
     }
   )
 }
@@ -576,7 +576,7 @@ module "cumulus" {
     {
       id              = "backgroundJobQueue",
       url             = aws_sqs_queue.background_job_queue.id,
-      execution_limit = 300
+      execution_limit = 500
     }
   ]
 }
---
+++ app/stacks/rds-cluster/tfvars/prod.tfvars
@@ -0,0 +1,4 @@
+# See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster
+
+min_capacity = 2
+max_capacity = 384
