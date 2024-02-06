508c82f660e98aba5fa41e8670803903056b64ae
Kris Stanton <ksnimpact@gmail.com>
2024-02-06 11:12:07 -0600

    created new rule for GE01 MSI L1B and changed the execution and message limit to 1400 #325

--- app/stacks/cumulus/main.tf
+++ app/stacks/cumulus/main.tf
@@ -161,7 +161,7 @@ resource "aws_cloudwatch_event_target" "background_job_queue_watcher" {
   arn  = module.cumulus.sqs2sfThrottle_lambda_function_arn
   input = jsonencode(
     {
-      messageLimit = 1000
+      messageLimit = 1400
       queueUrl     = aws_sqs_queue.background_job_queue.id
       timeLimit    = 60
     }
@@ -576,7 +576,7 @@ module "cumulus" {
     {
       id              = "backgroundJobQueue",
       url             = aws_sqs_queue.background_job_queue.id,
-      execution_limit = 1200
+      execution_limit = 1400
     }
   ]
 }
